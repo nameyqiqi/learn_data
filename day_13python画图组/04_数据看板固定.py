f97c1b0d0b81bc2438e5a5453535222e17648e83
nameyqiqi <107930355+nameyqiqi@users.noreply.github.com>
2023-09-15 19:53:13 +0800

    Delete day_13python画图组 directory

--- day_13python画图组/04_数据看板固定.py
+++
@@ -1,8 +0,0 @@
-from pyecharts.charts import Page
-
-Page.save_resize_html(
-    source='./tu/total.html',
-    cfg_file='./tu/chart_config.json',
-    dest='./数据看板finall.html.'
-
-)
--- day_13python画图组/1.py
+++
@@ -1,3 +0,0 @@
-list=[1,2,3,4]
-for item in reversed(list):
-    print(item)
